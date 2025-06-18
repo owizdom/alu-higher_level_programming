e44e9c2f0945d77e49ec378b912e46e579bfd754
owizdom <gokun4621@gmail.com>
2025-06-18 09:46:00 +0200

    Universal commit message

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-print("{}".format(
-    "".join(chr(letter) for letter in range(97, 123) if letter not in {101, 113})
-), end="")
+print("{}".format("".join(
+    chr(letter) for letter in range(97, 123) if letter not in {101, 113})), end="")
