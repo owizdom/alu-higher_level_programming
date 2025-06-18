616a722fc69138482771c89e23f32b56f26a6b52
owizdom <gokun4621@gmail.com>
2025-06-18 09:46:00 +0200

    Universal commit message

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str1 = str[39:66]
-str2 = str[107:111]
-str3 = str[0:6]
-print(f"{str1} {str2} {str3}")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str[39:67] + str[107:112] + str[0:6])
