343c11a99ccae44ff190f174ea95102f8ed35a3c
wizdom <154702154+owizdom@users.noreply.github.com>
2025-07-30 14:15:15 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,26 +1,8 @@
-#!/usr/bin/python3
-# 0-hbtn_status.py
-"""This module or script
-fetches https://alu-intranet.hbtn.io/status
-using the urllib library
-"""
+import urllib.request
 
-
-from urllib import request, error
-
-try:
-    url = 'https://intranet.hbtn.io/status'
-    with request.urlopen(url) as response:
-        response_body = response.read()
-        response_text = response_body.decode('utf-8')
-        print("Body response:")
-        print("\t- type:", type(response_body))
-        print("\t- content:", response_body)
-        print("\t- utf8 content:", response_text)
-except error.URLError:
-    custom_status = bytes("Custom status", encoding="utf-8")
-    custom_status_text = custom_status.decode('utf-8')
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+    body = response.read()
     print("Body response:")
-    print("\t- type:", type(custom_status))
-    print("\t- content:", custom_status)
-    print("\t- utf8 content:", custom_status_text)
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode('utf-8')))
